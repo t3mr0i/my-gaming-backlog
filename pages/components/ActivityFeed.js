f61aedc388902340ca90185157dc50a5c9c3a327
t3mr0i <detmers1@uni-bremen.de>
2023-03-19 21:06:12 +0100

    Init

---
+++ pages/components/ActivityFeed.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { db } from "../../firebase";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+
+const ActivityFeed = () => {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    const fetchActivities = async () => {
+      try {
+        const activitiesRef = collection(db, "activities");
+        const activitiesQuery = query(
+          activitiesRef,
+          orderBy("timestamp", "desc")
+        );
+        const querySnapshot = await getDocs(activitiesQuery);
+
+        const newActivities = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setActivities(newActivities);
+      } catch (error) {
+        console.error("Error fetching activities: ", error);
+      }
+    };
+
+    fetchActivities();
+  }, []);
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Activity Feed</h2>
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id} className="mb-2">
+            <span className="font-semibold">{activity.userName}</span> rated{" "}
+            <span className="font-semibold">{activity.gameName}</span>{" "}
+            {activity.rating} stars
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ActivityFeed;
---
+++ pages/components/SearchGame.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { collection, addDoc } from "firebase/firestore";
+import { db, auth } from "../../firebase";
+
+const SearchGame = ({ onAddGame }) => {
+  const [searchResults, setSearchResults] = useState([]);
+  const [gameTitle, setGameTitle] = useState(""); // Add this line
+
+  const searchGame = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`/api/searchGame?gameTitle=${gameTitle}`);
+      const data = await response.json();
+      setSearchResults(data);
+    } catch (error) {
+      console.error("Error fetching search results: ", error);
+    }
+  };
+
+  const addToBacklog = async (game) => {
+    try {
+      const docRef = await addDoc(
+        collection(db, "backlogs", auth.currentUser.uid, "games"),
+        {
+          gameId: game.id,
+          gameName: game.name,
+          gamePlatform: game.platform,
+        }
+      );
+      console.log("Document written with ID: ", docRef.id);
+    } catch (error) {
+      console.error("Error adding game to backlog: ", error);
+    }
+  };
+  return (
+    <form onSubmit={searchGame} className="mb-6">
+      <input
+        type="text"
+        value={gameTitle}
+        onChange={(e) => setGameTitle(e.target.value)} // Update this line
+        placeholder="Search for a game"
+        className="border border-gray-300 p-2 rounded w-full"
+      />
+
+      <ul>
+        {searchResults.map((result) => (
+          <li key={result.id}>
+            {result.name} - {result.platform}
+            <button onClick={() => addToBacklog(result)}>Add to backlog</button>
+          </li>
+        ))}
+      </ul>
+    </form>
+  );
+};
+
+export default SearchGame;
